e91b29f64c96dc0811ef3265d2fff37d353c1a2c
yuki_fujisawa <g031y148yuki@gmail.com>
2023-05-22 23:58:35 +0900

    Add テスト用のメソッド追加

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { MfInvoiceClient } from './app/mf-invoice-client';
 
-function createClient() {
+function createClient_() {
   return new MfInvoiceClient();
 }
 
 function authorizationUrl() {
-  Logger.log(createClient().getAuthorizationUrl());
+  Logger.log(createClient_().getAuthorizationUrl());
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function mfCallback(request: any) {
-  createClient().handleCallback(request);
+  createClient_().handleCallback(request);
+}
+
+function testbillingsList() {
+  Logger.log(createClient_().billings.list());
+}
+
+//テスト用
+function testQuotesList() {
+  Logger.log(createClient_().quotes.list());
+}
+
+function testPartnersList() {
+  Logger.log(createClient_().partners.list());
+}
+
+function testItemList() {
+  Logger.log(createClient_().items.list());
 }

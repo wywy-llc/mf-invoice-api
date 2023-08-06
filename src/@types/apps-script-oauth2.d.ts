c3e1e89ee3cbe5275814b9bda16ab1d4aa1c777e
yuki_fujisawa <g031y148yuki@gmail.com>
2023-08-06 18:10:25 +0900

    classは存在しないので、declareに修正

--- src/@types/apps-script-oauth2.d.ts
+++ src/@types/apps-script-oauth2.d.ts
@@ -3,29 +3,34 @@
  * OAuth2
  * https://github.com/googleworkspace/apps-script-oauth2
  */
-declare class OAuth2 {
-  static createService(service_name: string): OAuth2Service;
+declare namespace OAuth2 {
+  export declare function createService(service_name: string): OAuth2Service;
 }
 
 /**
  * OAuth2Service
- *
  */
-declare class OAuth2Service {
-  public setAuthorizationBaseUrl(url: string): OAuth2Service;
-  public setTokenUrl(url: string): OAuth2Service;
-  public setClientId(clientId: string): OAuth2Service;
-  public setClientSecret(clientSecret: string): OAuth2Service;
-  public setCallbackFunction(callbackFunction: string): OAuth2Service;
-  public setPropertyStore(
+declare namespace OAuth2Service {
+  export declare function setAuthorizationBaseUrl(url: string): OAuth2Service;
+  export declare function setTokenUrl(url: string): OAuth2Service;
+  export declare function setClientId(clientId: string): OAuth2Service;
+  export declare function setClientSecret(clientSecret: string): OAuth2Service;
+  export declare function setCallbackFunction(
+    callbackFunction: string
+  ): OAuth2Service;
+  export declare function setPropertyStore(
     propertyStore: GoogleAppsScript.Properties.Properties
   ): OAuth2Service;
-  public setScope(scope: string): OAuth2Service;
-  public getAuthorizationUrl(): string;
-  public getAccessToken(): string;
+  export declare function setScope(scope: string): OAuth2Service;
+  export declare function getAuthorizationUrl(): string;
+  export declare function getAccessToken(): string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public handleCallback(request: any): string;
-  public setCache(cache: GoogleAppsScript.Cache.Cache): OAuth2Service;
-  public setLock(lock: GoogleAppsScript.Lock.Lock): OAuth2Service;
-  public reset(): void;
+  export declare function handleCallback(request: any): string;
+  export declare function setCache(
+    cache: GoogleAppsScript.Cache.Cache
+  ): OAuth2Service;
+  export declare function setLock(
+    lock: GoogleAppsScript.Lock.Lock
+  ): OAuth2Service;
+  export declare function reset(): void;
 }
